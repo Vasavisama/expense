35c0526bbf5db05ebd0d873dadf0754ccd5a14eb
vasavisama <vasavisama2006@gmail.com>
2025-09-29 09:13:28 +0530

    mail

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::post('/register', [RegisterController::class, 'register']);
 
 
 
+
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
